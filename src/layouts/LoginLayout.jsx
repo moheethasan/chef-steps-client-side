9e121fb1d9d96a7f37623123da51251577ff86ed
majesty-01 <moheethasan4@gmail.com>
2023-05-02 14:06:43 +0600

    initital setup done

---
+++ src/layouts/LoginLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginLayout = () => {
+  return (
+    <div>
+      <nav></nav>
+      {/* outlet  */}
+      <footer></footer>
+    </div>
+  );
+};
+
+export default LoginLayout;
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <nav></nav>
+      <header></header>
+      {/* outlet home */}
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Main;
